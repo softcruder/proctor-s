bfec24e25cf16124bb0ecde1174259ce9c72e0b5
Nasirullah Oladipo <nasirullah.oladipo@raenest.com>
2024-06-26 16:42:18 +0100

    fix: config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/proctor-s",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
