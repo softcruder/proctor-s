253f60cc65f8e03138b262feea69cc47ef503d41
Softcruder(Personal) <oladipona17@gmail.com>
2024-07-14 00:12:59 +0100

    fix: config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,36 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+    {
+      key: 'X-Frame-Options',
+      value: 'DENY',
+    },
+    {
+      key: 'X-Content-Type-Options',
+      value: 'nosniff',
+    },
+    {
+      key: 'Referrer-Policy',
+      value: 'no-referrer',
+    },
+    {
+      key: 'X-XSS-Protection',
+      value: '1; mode=block',
+    },
+];
 const nextConfig = {
     // basePath: "/proctor-s",
     // output: "export",
+    async headers() {
+        return [
+          {
+            // Apply these headers to all routes in your application.
+            source: '/(.*)',
+            headers: securityHeaders,
+          },
+        ];
+      },
     reactStrictMode: true,
+    swcMinify: true,
     productionBrowserSourceMaps: true,
 };
 
